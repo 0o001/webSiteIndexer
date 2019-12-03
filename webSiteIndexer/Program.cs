f1a0347b700b7d642a0725d1111f5df771806e79
Mustafa Uzun <mustafa.uzun@std.izmirekonomi.edu.tr>
2019-12-03 22:21:25 +0300

    Initial commit

---
+++ webSiteIndexer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace webSiteIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (long ip = 0; ip < 4294967296; ip++)
+            {
+                try
+                {
+                    IPAddress ipAddress = new IPAddress(ip);
+                    string hostName = Dns.GetHostEntry(ipAddress).HostName;
+                    Console.WriteLine("IP: " + ipAddress);
+                    Console.WriteLine("Host Name: " + hostName);
+
+                }
+                catch { }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
